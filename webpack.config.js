59433247693504e8984eb51c7f31e820d1e91ed4
Matej Shanel <shanemat@ksu.edu>
2018-11-06 21:59:04 -0600

    Update configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
-    watchContentBase: true
-  }
+    mode: 'development',
+    entry: './src/scripts/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, "./dist"),
+      watchContentBase: true
+    }
 };
